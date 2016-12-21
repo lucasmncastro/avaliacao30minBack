236194145641d189ae9e99f5583f7efdf6a5570f
Lucas Castro <castro.lucas@gmail.com>
2016-12-21 16:59:23 -0300

    Preparando db para heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_filter :verify_authenticity_token
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161221194957) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "avaliacoes", force: :cascade do |t|
     t.string   "nome"
     t.integer  "estrelas"
