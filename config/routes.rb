e3643e86bb77a1440f13d2f712826b18d5213e8c
Lucas Castro <castro.lucas@gmail.com>
2016-12-21 17:18:01 -0300

    Criando rota /avaliar e tirando warning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :avaliacoes
   root to: "avaliacoes#index"
+  post "/avaliar" => "avaliacoes#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
